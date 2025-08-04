7327fb85ecd0e11975739cdea4fdfd68b7c00432
Fabrizio Ferroni <fabrizioferroni@outlook.com>
2025-08-03 22:20:21 -0300

    refactor: se elimina configuracion default nginx

--- conf/start.sh
+++ conf/start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Aseguramos que no haya una versión vieja
+rm -f /etc/nginx/conf.d/default.conf
+
 # Reemplaza la variable de entorno en el template y genera el archivo final
 envsubst '$API_URL' < /etc/nginx/nginx.template.conf > /etc/nginx/conf.d/default.conf
 
